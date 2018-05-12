fb93342f45d69d918f5ea34d0316d97b3f3ac51e
skrafi96@ <sk.rafi496@gmail.com>
2018-05-12 21:22:32 +0530

    export as pdf done

--- src/app/templates/app.templates.component.ts
+++ src/app/templates/app.templates.component.ts
@@ -1,6 +1,9 @@
 import { Component, ViewChild, ElementRef } from '@angular/core';
 import { Router } from '@angular/router'
 import { DetailsComponent } from '../details/app.details.component'
+import * as _ from 'underscore';
+import { saveAs } from 'file-saver';
+import * as jsPDF from 'jspdf';
 
 
 @Component({
@@ -18,4 +21,29 @@ export class TemplatesComponent {
     data = localStorage.getItem("dynamic");
     test = localStorage.getItem("test"); 
     lastname = localStorage.getItem("lastname");  
+
+    @ViewChild('content') content: ElementRef;
+    public downloadPDF() {
+      let doc = new jsPDF();
+  
+      let specialElementHandler = {
+        '#editor': function (element, renderer) {
+          return true;
+        }
+      };
+      let content = this.content.nativeElement;
+      doc.fromHTML(content.innerHTML, 15, 15, {
+        'widht': 190,
+        'elementHandler': specialElementHandler
+      });
+  
+      doc.save('test.PDF');
+      doc.save('test.doc');
+    }
+    private modals: any[] = [];
+    open(id: string) {
+      // open modal specified by id
+      let modal = _.findWhere(this.modals, { id: id });
+      modal.open();
+    }
 }
